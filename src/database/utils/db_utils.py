000654154e878d536942ef92ef9341bd81028c3c
Mr. Stallion <mrstallion@nobody.nowhere.fauxdomain.ext>
2023-09-22 13:20:36 -0700

    minor

--- src/database/utils/db_utils.py
+++ src/database/utils/db_utils.py
@@ -5,7 +5,7 @@ import os
 def connect_to_db(
     username=os.environ.get('DB_USERNAME', 'root'),
     password=os.environ.get('DB_PASSWORD', 'root'),
-    db_name=os.environ.get('DB_DATABASE', 'e621_rising'),
+    db_name=os.environ.get('DB_DATABASE', 'dataset_rising'),
     host=os.environ.get('DB_HOST', 'localhost'),
     port=os.environ.get('DB_PORT', 27017)
 ):
